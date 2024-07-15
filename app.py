72906e95d77939f2e857169f8c12d9fe9fa92689
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2024-07-15 21:16:18 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from config import Config
+from models import db, User
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User(username=username)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        flash('User registered successfully!')
+        return redirect(url_for('index'))
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.check_password(password):
+            flash('Logged in successfully!')
+            return redirect(url_for('index'))
+        flash('Invalid username or password')
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
